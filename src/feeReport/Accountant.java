7b5c20ff400398c557c08ec2eb9d80c88e5901d7
Pooja Yadav <100533707+YadavPooja-29@users.noreply.github.com>
2022-03-06 03:02:26 +0530

    Add files via upload

---
+++ src/feeReport/Accountant.java
@@ -0,0 +1,44 @@
+package com.corejava.feereport;
+
+public class Accountant
+{
+	private int id;
+	private String name,password,email,contactno;
+	public Accountant() {}
+	public Accountant(String name, String password, String email, String contactno)
+	{
+		super();
+		this.name = name;
+		this.password = password;
+		this.email = email;
+		this.contactno = contactno;
+	}
+	public int getId()
+	{
+		return id;
+	}
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public String getPassword()
+	{
+		return password;
+	}
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+	public String getEmail()
+	{
+			return email;
+	}
+}
